02b5d6c9b39115eff35250b04c813f3220bf28fa
Muhammad Hassan <hassantariq.8631@gmail.com>
2016-02-26 15:57:28 -0600

    Initial

---
+++ Scripts/app/app.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', ['ngMaterial']);
+
+    app.controller('autocompleteController', function () {
+        
+        var vm = this;
+
+        vm.selectedItem = "";
+
+        vm.dataList = [{ text: 'ABC', value: 1 },
+                        { text: 'XYZ', value: 2 },
+                        { text: 'Foo', value: 3 },
+                        { text: 'Bar', value: 4 }];
+
+        vm.showSelectedItem = function (valueReturnByDirective) {
+            vm.selectedItem = valueReturnByDirective;
+        }
+
+    });
+
+})();
---
+++ Scripts/app/autoSearchDirective.js
@@ -0,0 +1,76 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('searchCombo', searchCombo);
+
+    function searchCombo() {
+        var directive = {
+            restrict: 'EA',
+            templateUrl: '/scripts/app/autoSearchDirective.html',
+            scope: {
+                data: '=',
+                filtersearch: '&'
+            },
+            controller: autoCompleteController,
+            controllerAs: 'vm',
+            bindToController: true
+        };
+
+        return directive;
+    }
+
+    autoCompleteController.$inject = [];
+
+    function autoCompleteController() {
+
+        var vm = this;
+
+        init();
+
+        /////////////////////Implementation///////////////////////////////
+
+        function init() {
+            vm.datalist = vm.data; 
+            vm.filterSearch = vm.filtersearch;
+
+            vm.config = {};
+            vm.config.disabled = false;
+            vm.config.noCache = false;
+            vm.searchText = "";
+            vm.selectedItem = "";
+            vm.selectedItemChange = selectedItemChange;
+            vm.searchTextChange = searchTextChange;
+            vm.querySearch = querySearch;
+
+        }
+
+        function querySearch(query) {
+            var results = query ? vm.datalist.filter(createFilterFor(query)) : vm.datalist;
+            return results;
+        }
+
+        function createFilterFor(query) {
+            var lowercaseQuery = angular.lowercase(query);
+
+            return function filterFn(data) {
+                return (angular.lowercase(data.text).indexOf(lowercaseQuery) === 0);
+            };
+
+        }
+
+        function searchTextChange(text) {
+            console.log('Text changed to ' + text);
+        }
+
+        function selectedItemChange(item) {
+            if (item)
+                vm.filterSearch()(vm.selectedItem);
+            else
+                vm.filterSearch()("");
+        }
+
+    }
+
+})();
